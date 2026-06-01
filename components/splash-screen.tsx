9f74007a00159241c9cb90daa6f8cfbde6d2cb48
Axel Sandoval <axesan917@gmail.com>
2026-06-01 08:49:30 -0700

    feat: add Turno wordmark next to logo in splash

--- components/splash-screen.tsx
+++ components/splash-screen.tsx
@@ -77,14 +77,17 @@ export function SplashScreen() {
         }}
       />
 
-      {/* Logo */}
-      <div data-splash-logo style={{ opacity: 0, position: 'relative', zIndex: 1 }}>
+      {/* Logo + wordmark */}
+      <div data-splash-logo style={{ opacity: 0, position: 'relative', zIndex: 1, display: 'flex', alignItems: 'center', gap: 18 }}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="/logotrans.png"
           alt="Turno"
           style={{ height: 120, width: 'auto', filter: 'brightness(0) invert(1)' }}
         />
+        <span style={{ fontSize: 52, fontWeight: 700, color: '#ebebeb', letterSpacing: '-0.03em', fontFamily: 'var(--font-geist-sans)', lineHeight: 1 }}>
+          Turno
+        </span>
       </div>
     </div>
   )
